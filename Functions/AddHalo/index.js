4329c7e27cab19bceac88b19f5d2b18f37f95875
Danielsiy <dsiy@wisc.edu>
2020-05-23 00:21:31 -0500

    Added create item with corresponding columns

--- Functions/AddHalo/index.js
+++ Functions/AddHalo/index.js
@@ -1,6 +1,17 @@
 'use strict';
 const snoowrap = require('snoowrap');
+
+
 var AWS = require("aws-sdk");
+
+let aswconfig = {
+    "region": "us-east-2",
+    // "endpoint": "https://dynamodb.us-east-2.amazonaws.com",
+    "accessKeyId": process.env.KEY_ID, 
+    "secretAccessKey": process.env.SECRET_KEY_ID
+}
+AWS.config.update(aswconfig);
+
 
 var dynamodb = new AWS.DynamoDB();
 
@@ -12,7 +23,14 @@ const reddit = new snoowrap({
     password: process.env.PASSWORD
 });
 
+
+// Create the DynamoDB service object
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+
+
 exports.handler = async (body, context, callback) => {
+
     if (!validateRequest(body)) {
         return "Invalid body.";
     }
@@ -20,6 +38,28 @@ exports.handler = async (body, context, callback) => {
     const commentId = body.commentId;
     const comment = await reddit.getComment(commentId).fetch();
     const parent = await getParent(comment);
+   
+    var params = {
+        TableName:"Halo",
+        Item:{
+            "initiator": comment.author.name,
+            "recipient": parent.author.name,
+            "commentId": commentId,
+            "subreddit": comment.subreddit.display_name,
+            "id": Math.round(Math.random()*100000).toString()
+        }
+    };
+    
+    console.log("Adding a new item...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+ 
+
 };
 
 async function getParent(comment) {
